b5c896d78d1e1101fb132123e96b0150fe5c205b
KumarGudipalli <kumargudipalli97@gmail.com>
2022-07-12 09:19:44 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Input from './Component/Input';
+import Geolocation from "./Component/geolocation"
+import { useState } from 'react';
+import Search from './Component/Search';
+import TimeChart from './Component/TimeChart';
+function App() {
+ 
+
+
+  return (
+    <div className='App'>  
+    <div className='container'>
+<Search/>
+      </div>  
+   
+
+    </div>
+  );
+}
+
+
+// 866dd4234b8320abb4d449f5c3efab41
+export default App;
